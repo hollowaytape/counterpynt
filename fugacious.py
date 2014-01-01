787bac97994b0c53ced145b8c0f78fc8f3421f2c
Max Silbiger <tsmaxweliot@gmail.com>
2013-12-31 21:58:51 -0500

    Rests and extends, split input function into an input one and format_notes().

--- fugacious.py
+++ fugacious.py
@@ -42,42 +42,47 @@ class Interval:
         self.lo = lo
         self.hi = hi
 
-        if "#" in self.lo and "b" in self.hi:                 # Sharps/flats can't coexist, so replace the higher note.
-            self.hi = notes_sharps[notes_flats.index(hi)]     # Find the corresponding sharp note to the flat.
-        elif "b" in self.lo and "#" in self.hi:
-            self.hi = notes_flats[notes_sharps.index(hi)]
+        if "x" in self.lo or "x" in self.hi:                  # Dummy interval between one or more "rests".
+            self.name = "unaccompanied note"
+            self.weight = 0
 
-        if "#" in self.lo or "#" in self.hi:
-            lo_index = notes_sharps.index(self.lo)
-            hi_index = notes_sharps.index(self.hi)
-            if lo_index > hi_index:
-                self.semitone_distance = hi_index + (len(notes_sharps) - lo_index)   # If the hi note is "lower", it's
-            else:                                                                    # higher but with a lower letter.
-                self.semitone_distance = hi_index - lo_index
         else:
-            lo_index = notes_flats.index(self.lo)
-            hi_index = notes_flats.index(self.hi)
-            if lo_index > hi_index:
-                self.semitone_distance = hi_index + (len(notes_flats) - lo_index)
+            if "#" in self.lo and "b" in self.hi:                 # Sharps/flats can't coexist, so replace the higher note.
+                self.hi = notes_sharps[notes_flats.index(hi)]     # Find the corresponding sharp note to the flat.
+            elif "b" in self.lo and "#" in self.hi:
+                self.hi = notes_flats[notes_sharps.index(hi)]
+
+            if "#" in self.lo or "#" in self.hi:
+                lo_index = notes_sharps.index(self.lo)
+                hi_index = notes_sharps.index(self.hi)
+                if lo_index > hi_index:
+                    self.semitone_distance = hi_index + (len(notes_sharps) - lo_index)   # If the hi note is "lower", it's
+                else:                                                                    # higher but with a lower letter.
+                    self.semitone_distance = hi_index - lo_index
             else:
-                self.semitone_distance = hi_index - lo_index
+                lo_index = notes_flats.index(self.lo)
+                hi_index = notes_flats.index(self.hi)
+                if lo_index > hi_index:
+                    self.semitone_distance = hi_index + (len(notes_flats) - lo_index)
+                else:
+                    self.semitone_distance = hi_index - lo_index
 
-        self.number = interval_number[notes_naturals.index(self.hi[0]) - notes_naturals.index(self.lo[0])]
+            self.number = interval_number[notes_naturals.index(self.hi[0]) - notes_naturals.index(self.lo[0])]
 
-        distance_from_major = (self.semitone_distance - diatonic_distance[interval_number.index(self.number)])
-        if self.number in ("unison", "fourth", "fifth", "octave"):
-            self.quality = interval_quality_perfect[distance_from_major]
-        else:
-            self.quality = interval_quality_imperfect[distance_from_major]
+            distance_from_major = (self.semitone_distance - diatonic_distance[interval_number.index(self.number)])
+            if self.number in ("unison", "fourth", "fifth", "octave"):
+                self.quality = interval_quality_perfect[distance_from_major]
+            else:
+                self.quality = interval_quality_imperfect[distance_from_major]
 
-        self.name = self.quality + ' ' + self.number
+            self.name = self.quality + ' ' + self.number
 
-        if self.quality == "perfect":      # Perfect consonances receive the highest weight.
-            self.weight = 2
-        elif self.name in ['minor third', 'major third', 'minor sixth', 'major sixth']:  # Imperfects receive lower.
-            self.weight = 1
-        else:                              # Dissonances are negative.
-            self.weight = -1
+            if self.quality == "perfect":      # Perfect consonances receive the highest weight.
+                self.weight = 2
+            elif self.name in ['minor third', 'major third', 'minor sixth', 'major sixth']:  # Imperfects receive lower.
+                self.weight = 1
+            else:                              # Dissonances are negative.
+                self.weight = -1
 
     def __repr__(self):
         return self.name
@@ -86,51 +91,59 @@ class Interval:
 songbook = {"a_scale": ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
             "c_scale": ['C', 'D', 'E', 'F', 'G', 'A', 'B'],
             "rowboat": ['C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'D', 'E', 'E', 'E',   # "Row, Row, Row Your Boat"
-                        'E', 'E', 'D', 'E', 'E', 'F', 'G', 'G', 'G', 'G', 'G', 'G',   # Each note is a triplet, no rests.
+                        'E', 'E', 'D', 'E', 'E', 'F', 'G', 'G', 'G', 'x', 'x', 'x',   # Each note is a triplet, no rests.
                         'C', 'C', 'C', 'G', 'G', 'G', 'E', 'E', 'E', 'C', 'C', 'C',   # Kinda awkward.
-                        'G', 'G', 'F', 'E', 'E', 'D', 'C', 'C', 'C', 'C', 'C', 'C']   # Rounds are kinda like fugues though.
-}
+                        'G', 'G', 'F', 'E', 'E', 'D', 'C', 'C', 'C', 'x', 'x', 'x']   # Rounds are kinda like fugues.
+            }
+
+
+def format_melody(notes):                                                # Sanity checks are now part of one function.
+    for note in notes:
+        if note in notes_corrected:
+            note = notes_corrected[note]                                 # Let the user know?
+        if note == "-":                                                  # Syntax for a previous note continuing.
+            note = notes[notes.index(note) - 1]                          # Replace with the previous note's value.
+    return notes
 
 
 def manual_input():
     user_subject = []
-    print "Input the subject of your fugue, one note at a time. 'x' to finish."
+    print "Input a melody, one note at a time.\n'x' is a rest, '-' extends the previous note, 'end' to finish."
     while True:
         note_input = raw_input("> ")
-        if note_input[0] not in 'ABCDEFGx':
+        if note_input[0] not in 'ABCDEFGx- ':
             raise ValueError('Invalid note input.')
-        if note_input == "x":
+        if note_input == "end":
+            format_melody(user_subject)
             find_fugue(user_subject)
-        if note_input in notes_corrected:
-            note_input = notes_corrected[note_input]
-            print "%s has been renamed %s." % (note_input, notes_corrected[note_input])
 
         user_subject.append(note_input)
         print user_subject
 
 
-def load_melody():                           # Not currently functional.
+def load_melody():
     print "Enter the name of the melody."
     melody = raw_input("> ")
-    find_fugue(songbook[melody])                   # This interprets the input string as a melody without finding the variable...
+    find_fugue(songbook[melody])
 
 
 def find_fugue(user_subject):
     score_list = []
-    for note_index in range(0, len(user_subject)):        # Ending a fugue is not something for computers to do.
+    for note_index in range(0, len(user_subject)):       # Ending a fugue is not something for computers to do.
         user_subject.append(user_subject[note_index])    # Instead, we'll simply repeat the subject twice.
     length = len(user_subject) / 2
     for stagger in range(1, length):                     # Test every amount of stagger from the 2nd to last notes.
         weight_sum = 0
         for note_index in (0, length):                   # Sum the weights of each interval.
             i = Interval(user_subject[note_index], user_subject[note_index + stagger])
-            print i.lo, i.hi, i
+            print i.lo, i.hi, i                          # rowboat is printing C * every note in the piece...
             weight_sum += i.weight
         score_list.append(weight_sum)
 
     print user_subject
     print score_list
-    print "The most consonant fugue would enter %s notes after the first." % (score_list.index(max(score_list)))
+    print "The most consonant fugue would enter %s notes after the first." % (score_list.index(max(score_list)) + 1)
+    # Multiple options for where to begin the fugue? A list of all the maxima would be nice to have.
 
 print "Fugacious v0.1"
 print "a) Manual input b) Name of a hard-coded melody"
